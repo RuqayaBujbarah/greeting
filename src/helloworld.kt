f5c2a81af99e854061a7d23c05fffd2b9dbf72cd
RuqayaBujbarah <Ruqayatb95@hotmail.com>
2021-09-19 22:54:16 +0300

    permission

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,18 +1,6 @@
 fun main(){
-    val age = 19
-    var check = 1
-    while (check!=0){
-        try{
-        print("Enter your age: ")
-        if(readLine()!!.toInt()>=age){
-            println("Welcome!")
-        }
-        else{
-            println("You are not old enough...")
-        }
-        check = 0
-    }
-        catch (e: Exception){
-        println("Please enter a number!")
-    }}
+    val message = "Hello user, welcome to my program!"
+    print("Please enter your name:  ")
+    val userName = readLine()
+    println(message.replace("user", userName!!))
 }
